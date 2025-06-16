42be96a29c03b9f7ebc62ce00f4e7a8e81734f63
Arthur Adriel <arthurtorquato25@gmail.com>
2025-06-15 23:08:44 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,16 +3,12 @@ from codigo import soma, subtrai, multiplica, divide
 
 def test_soma():
     assert soma(2, 3) == 5
-
 def test_subtrai():
     assert subtrai(5, 3) == 2
-
 def test_multiplica():
     assert multiplica(4, 2) == 8
-
 def test_divide():
     assert divide(10, 2) == 5
-
 def test_divide_por_zero():
     with pytest.raises(ValueError):
         divide(10, 0)
