db601f2a91479663316cbf6dc7ad01130ebc6ebc
Arthur Adriel <arthurtorquato25@gmail.com>
2025-06-15 23:09:11 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,12 +1,9 @@
 def soma(a, b):
     return a + b
-
 def subtrai(a, b):
     return a - b
-
 def multiplica(a, b):
     return a * b
-
 def divide(a, b):
     if b == 0:
         raise ValueError("Divisão por zero não é permitida.")
